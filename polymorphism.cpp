aa6479f4b187cb1efd2d7ea97fda9b7b5df9c16f
Eman Siddiqui <emansiddiqui2781@gmail.com>
2026-04-29 03:56:15 -0700

    Add files via upload
    This project demonstrates function overloading in C++ by calculating the area of different shapes using the same function name.

---
+++ polymorphism.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Area{
+	public:
+	int	calculateArea(int r){
+		return 3.14*r*r;
+	}
+	int calculateArea(int l,int b){
+		return l*b;
+	}
+	
+};
+
+int main(){
+	Area A1,A2;
+	cout<<A1.calculateArea(2)<<endl;
+	cout<<A2.calculateArea(3,9)<<endl;
+	return 0;
+}
